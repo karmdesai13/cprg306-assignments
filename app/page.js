70e0935123750592e4a94ae09c67d2dd7f8f21e7
karmdesai13 <karmdushyantkumar.desai@edu.sait.ca>
2023-09-17 17:48:21 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
-
+import Link from "next/link";
 
 export default function StudentInfo() {
   return (
     <div>
       <p>Karm Desai</p>
       <p>CPRG 306 A</p>
-      <link ></link>
+      <Link href="https://github.com/karmdesai13/cprg306-assignments.git">Github Link</Link>
     </div> 
     
-  )
+  );
 }
