45b676b6850c12eae45b467f11f542f7bf0284ec
karmdesai13 <karmdushyantkumar.desai@edu.sait.ca>
2023-09-26 10:44:05 -0600

    completed page.js

---
+++ app/week3/page.js
@@ -0,0 +1,11 @@
+import ItemList from "./item-list";
+
+export default function Page() {
+    return (
+        <div>
+            <h1>Shopping List</h1>
+            <ItemList />
+
+        </div>
+    );
+}
