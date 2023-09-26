2f301a0dd8a2e3cef46d464cd26edbd095a6ea7d
karmdesai13 <karmdushyantkumar.desai@edu.sait.ca>
2023-09-26 10:55:17 -0600

    Update item.js

--- app/week3/item.js
+++ app/week3/item.js
@@ -1,7 +1,10 @@
 export default function Item({ name,quantity,category}) {
+
+    const CapitalName = name.charAt(0).toUpperCase() + name.slice(1);
+
     return (
         <div className="bg-blue-900 border border-white rounded m-3 p-4">
-            <h1 className="text-2xl text-white">{name}</h1>
+            <h1 className="text-2xl text-white">{CapitalName}</h1>
             <p> Buy {quantity} from {category}</p>
         </div>
     );
