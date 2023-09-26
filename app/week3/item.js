8088aacefe812e5306e7b9d57863071cfd10579d
karmdesai13 <karmdushyantkumar.desai@edu.sait.ca>
2023-09-26 10:53:26 -0600

    item.js updated with tailwind css

--- app/week3/item.js
+++ app/week3/item.js
@@ -1,7 +1,7 @@
 export default function Item({ name,quantity,category}) {
     return (
-        <div>
-            <h1>{name}</h1>
+        <div className="bg-blue-900 border border-white rounded m-3 p-4">
+            <h1 className="text-2xl text-white">{name}</h1>
             <p> Buy {quantity} from {category}</p>
         </div>
     );
