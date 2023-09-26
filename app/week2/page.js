3aaebfe1704fff05f02aeb3913bc403398211305
karmdesai13 <karmdushyantkumar.desai@edu.sait.ca>
2023-09-26 10:31:06 -0600

    link of week3 added to week2

--- app/week2/page.js
+++ app/week2/page.js
@@ -8,7 +8,7 @@ export default function ShoppingList() {
       <h1 class="text-4xl text-blue-500">My Shopping List</h1>
       <hr></hr>
       <StudentInfo/>
-      
+      <Link href="./week3" className="text-white m-4 hover:text-red-500 underline">Week3</Link>
     </div>
   );
 }
