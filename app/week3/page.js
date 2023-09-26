d3bd473ba96182b9fefa9715823fa64f2452f224
karmdesai13 <karmdushyantkumar.desai@edu.sait.ca>
2023-09-26 10:49:23 -0600

    added tailwind css to page.js

--- app/week3/page.js
+++ app/week3/page.js
@@ -2,8 +2,8 @@ import ItemList from "./item-list";
 
 export default function Page() {
     return (
-        <div>
-            <h1>Shopping List</h1>
+        <div className="bg-black text-white p-2  m-1">
+            <h1 className="text-2xl text-center underline p-3">Shopping List</h1>
             <ItemList />
 
         </div>
