3f8738d22dff8318f19817c1d31a287864d99d46
karmdesai13 <karmdushyantkumar.desai@edu.sait.ca>
2023-10-13 21:35:46 -0600

    added tailwind css to week5

--- app/week5/item-list.js
+++ app/week5/item-list.js
@@ -16,24 +16,26 @@ export default function ItemList() {
 
     return(
 
-        <div className="flex">
+        <main className="">
 
-        <h1 className="text-xl text-white p-3 m-0 h-10 ">Sort by:</h1>
-        <button onClick={() => setSortby("name")} 
-                className="bg-blue-500 text-white p-3 m-2  hover:bg-orange-500 border border-yellow-600 rounded h-10"
+        <div className=" text-center">
+          <h1 className="text-xl text-white p-3  h-10 ">Sort by:</h1>
+          <button onClick={() => setSortby("name")} 
+                className="bg-blue-500 text-white py-3 px-4 m-2  hover:bg-orange-500 border rounded h-10 font-mono"
                 >Sort by Name</button> 
-        <button onClick={() => setSortby("category")} 
-             className="bg-blue-500 text-white p-3 m-2  hover:bg-orange-500 border border-yellow-600 rounded h-10 ">Sort by Category</button>
+          <button onClick={() => setSortby("category")} 
+             className="bg-blue-500 text-white p-3 m-2  hover:bg-orange-500 border-2 rounded h-10 font-mono ">Sort by Category</button>
         
-         
-        <ul className="m-5 p-30 space-y-0.5 ">
+        </div>
+        <div className="m-4  ">
+         <ul className=" rounded-full text-center space-y-2 bg-purple-500 text-black border-white border-3 p-4" >
             {sortedItems.map((item) => (
                 <li key={item.id}>
                     {item.name} - {item.category}
                 </li>
             ))}</ul>
-                      
-        </div>
+        </div>               
+        </main>
 
     );
 
--- app/week5/page.js
+++ app/week5/page.js
@@ -3,7 +3,7 @@ import ItemList from "./item-list";
 export default function Page() {
     return (
         <div className="bg-black text-white p-2  m-1">
-            <h1 className="text-3xl text-center underline p-3">Shopping List</h1>
+            
             <ItemList />
 
         </div>
