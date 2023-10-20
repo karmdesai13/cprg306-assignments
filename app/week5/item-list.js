1cd6e5b728f0f9a949253399de1d77c94787db56
karmdesai13 <karmdushyantkumar.desai@edu.sait.ca>
2023-10-20 10:03:44 -0600

    added css

--- app/week5/item-list.js
+++ app/week5/item-list.js
@@ -32,7 +32,7 @@ export default function ItemList() {
           <h1 className="text-xl text-white p-3 h-10">Sort By:</h1>
           <button onClick={() => setSortby("name")} 
                 className={`bg-blue-500 text-white p-2 px-4 m-2  hover:bg-orange-500 hover:text-xl  border-2 rounded h-10 font-mon 
-                ${ sortby === "name" ? "bg-red-500 underline ": "" }`}
+                ${ sortby === "name" ? "bg-green-500 underline ": "" }`}
                 >Sort by Name
           </button> 
           <button onClick={() => setSortby("category")} 
@@ -42,7 +42,8 @@ export default function ItemList() {
           </button>
 
           <button onClick={() => {setSortby("group")}}
-                className="bg-blue-500 text-white p-2 m-2 hover-bg-orange-500 border-2 rounded h-10 font-mono">
+                className={`bg-blue-500 text-white p-2 m-2  hover:bg-orange-500 hover:text-xl border-2 rounded h-10 font-mono 
+                ${sortby === "group"? "bg-green-500 underline":""}`}>
                 Group by Category (Working Now) 
           </button>
         
@@ -50,12 +51,12 @@ export default function ItemList() {
        
 
         {sortby==="group"?(
-            <div className="m-4">
+            <div className="m-4 ">
                 {Object.entries(groupedItems).map(([category, items]) => (
                     <div key={category} className="">
                         
-                        <ul>
-                            <h3 className="capitalize text-2xl">{category}</h3>
+                        <ul className="">
+                            <h3 className="capitalize text-2xl ">{category} :</h3>
                             {items.map((item) => (
                                 <li key={item.id} className="bg-blue-800
                                 m-4 w-[270px] text-xl border-[2px]
@@ -69,11 +70,11 @@ export default function ItemList() {
             </div>
         ):(<ul className="m-10">
             {sortedItems.map((item) => (
-                <li key={item.id} className="m-3 bg-blue-900 p-3 
-                 w-[350px] ml-[300px] hover:bg-orange-500 
-                 hover:">
-                    <p className="text-xl"> Pick {item.name}</p>
-                    <p className=""> From {item.category}</p>
+                <li key={item.id} className="m-3 bg-blue-900 p-3
+                w-[300px] ml-[300px] hover:bg-orange-500
+                border-[2px] border-white rounded-full text-center hover:text-2xl">
+                    <p className="">Pick {item.name}</p>
+                    <p className="">From {item.category}</p>
                 </li>
             ))}
         </ul>)}
